8c0f2ded8900e095c2ce35b0a8b1825c9276d398
lin <209758171@163.com>
2020-01-06 11:44:35 +0800

    修改测试代码

--- test_dlist.c
+++ test_dlist.c
@@ -25,6 +25,16 @@ int main(void) {
     listInsertNode(ins,listFirst(ins),"5678",1);
     listAddNodeTail(ins,"90");
 
+    struct listIter* iter =  listGetIterator(ins,AL_START_HEAD);
+
+    sds* sds2 = sdsnew(NULL);
+    listNode* node = NULL;
+    while((node = listNext(iter))!=NULL){
+        sdscat(sds2,node->value);
+    }
+
+    printf("sds2=%s\n",sds2);
+    test_cond("list insert node", memcmp(sds2,"1234567890",10)==0 );
 
     test_report();
     return 0;
